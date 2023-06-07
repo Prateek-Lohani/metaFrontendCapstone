42b5beac93b9bfbe6db2ff7212cc8463051500f5
Prateek Lohani <prateeklohani105@gmail.com>
2023-06-07 16:49:03 +0530

    Open Graph API commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <>
+    <img src="" alt="img"></img>
+    <nav>
+      <ul>
+        <li><a href="Home">Home</a></li>
+        <li><a href="Products">Products</a></li>
+        <li><a href="About">About</a></li>
+        <li><a href="Contact Us">Contact Us</a></li>
+      </ul>
+    </nav>
+    </>
+  )
+}
+
+export default Nav
